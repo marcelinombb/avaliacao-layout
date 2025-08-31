d6d740544faf51ea5812796f3275913728956c7c
marcelinombb <marcelino.m.b.braga@gmail.com>
2025-08-31 16:22:18 -0300

    feat: geração de tipos

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,17 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import json from "@rollup/plugin-json";
+import dts from "rollup-plugin-dts";
 
 export default [
+  {
+    input: "src/index.js", // ponto de entrada da sua lib
+    output: {
+      file: "dist/index.esm.js",
+      format: "esm", // gera um bundle ESM
+    },
+    plugins: [resolve(), commonjs(), json()],
+  },
   // Bundle para Node.js (CommonJS)
   {
     input: "src/index.js",
@@ -20,8 +29,16 @@ export default [
     output: {
       file: "dist/index.umd.js",
       format: "umd", // ou "iife"
-      name: "MyLib", // nome global no browser
+      name: "AvaliacaoLayout", // nome global no browser
     },
     plugins: [resolve(), commonjs(), json()],
+  },
+  {
+    input: "types/index.d.ts", // gerado pelo tsc
+    output: {
+      file: "dist/index.d.ts",
+      format: "es",
+    },
+    plugins: [dts()],
   },
 ];
